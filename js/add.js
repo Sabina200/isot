c632b0ffe8fc62865824b95bfe0fe749057d7b55
Sabina200 <kubanychbekovasabina11@gmail.com>
2021-12-29 16:06:30 +0600

    first commit

---
+++ js/add.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){ 
+    $("#menu li a").click(function(){ 
+        $("#menu li a").removeClass('active'); 
+        $(this).addClass('active') 
+ 
+        var targetedItems = $(this).data('target'); 
+        if(targetedItems === 'all'){ 
+            $(".care").fadeIn(); 
+        }else{ 
+            $(".care").hide(); 
+            $("."+targetedItems).fadeIn(); 
+        } 
+    }) 
+});
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Спасибо!";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Ошибка";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+});
+  
+// helper function for sending an AJAX request
+  
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+}
